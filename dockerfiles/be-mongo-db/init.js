6d531e928cf11b0cd96c65f2c29348b4827472c1
Kevin Quinn <kquinn@redhat.com>
2025-01-02 11:49:30 +0000

    Update init.js

--- dockerfiles/be-mongo-db/init.js
+++ dockerfiles/be-mongo-db/init.js
@@ -8,17 +8,18 @@ db.songs.drop();
 db.createCollection('songs');
 
 // Insert song data into the 'songs' collection
+// Insert song and artist data into the 'songs' collection
 db.songs.insertMany([
-    { songNumber: 1, mp3_filename: 'song1.mp3', song_name: 'Born to Run' },
-    { songNumber: 2, mp3_filename: 'song2.mp3', song_name: 'Material Girl' },
-    { songNumber: 3, mp3_filename: 'song3.mp3', song_name: 'Superstition' },
-    { songNumber: 4, mp3_filename: 'song4.mp3', song_name: 'Ain\'t Nobody' },
-    { songNumber: 5, mp3_filename: 'song5.mp3', song_name: 'Come on Eileen' },
-    { songNumber: 6, mp3_filename: 'song6.mp3', song_name: 'Summertime Sadness' },
-    { songNumber: 7, mp3_filename: 'song7.mp3', song_name: 'Too Sweet' },
-    { songNumber: 8, mp3_filename: 'song8.mp3', song_name: 'Theme From Shaft' },
-    { songNumber: 9, mp3_filename: 'song9.mp3', song_name: 'Fortnight' },
-    { songNumber: 10, mp3_filename: 'song10.mp3', song_name: 'I Had Some Help' }
+    { songNumber: 1, mp3_filename: 'song1.mp3', song_name: 'Beautiful Day', artist_name: 'U2' },
+    { songNumber: 2, mp3_filename: 'song2.mp3', song_name: 'Shivers', artist_name: 'Ed Sheeran' },
+    { songNumber: 3, mp3_filename: 'song3.mp3', song_name: 'Take Your Mama', artist_name: 'Scissor Sisters' },
+    { songNumber: 4, mp3_filename: 'song4.mp3', song_name: 'Vampire', artist_name: 'Olivia Rodrigo' },
+    { songNumber: 5, mp3_filename: 'song5.mp3', song_name: 'Try', artist_name: 'Pink' },
+    { songNumber: 6, mp3_filename: 'song6.mp3', song_name: 'Enjoy the Silence', artist_name: 'Depeche Mode' },
+    { songNumber: 7, mp3_filename: 'song7.mp3', song_name: 'About Damn Time', artist_name: 'Lizzo' },
+    { songNumber: 8, mp3_filename: 'song8.mp3', song_name: 'Sabotage', artist_name: 'Beastie Boys' },
+    { songNumber: 9, mp3_filename: 'song9.mp3', song_name: 'Believe', artist_name: 'Cher' },
+    { songNumber: 10, mp3_filename: 'song10.mp3', song_name: 'Blinding Lights', artist_name: 'The Weekend' }
 ]);
 
 // Drop and recreate the 'players' collection, starting with empty data
